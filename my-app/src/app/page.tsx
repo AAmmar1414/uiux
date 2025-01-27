5670416a8931431d7c2a9b03ce6aab372c2c46fc
AMMAR IQBAL <147102569+AAmmar1414@users.noreply.github.com>
2025-01-28 01:15:47 +0500

    up
    up

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,29 +1,20 @@
 
-import Hero from "./components/Hero";
-import LogoSection from "./components/logosection";
-import FeaturedProducts from "./components/FeaturedProduct";
-import TopProducts from "./components/Topproducts";
-import ImageCard from "./components/ImageCard";
-import ProductGridWithCart from "./components/Productgird";
+import Hero from './components/Hero';
+import LogoSection from './components/logosection';
+import FeaturedProducts from './components/FeaturedProduct';
+import TopProducts from './components/Topproducts';
+import ImageCard from './components/ImageCard';
 
-
-
-
-
-
-export default  function Home() {
-
-
+const Page = () => {
   return (
-
-
-
-    <div >
-      <Hero/>
-      <LogoSection/>
-      <FeaturedProducts/>
-      <TopProducts/>
-      <ImageCard/>
-     </div>
+    <div>
+      <Hero />
+      <LogoSection />
+      <FeaturedProducts />
+      <TopProducts />
+      <ImageCard />
+    </div>
   );
-}
+};
+
+export default Page;
