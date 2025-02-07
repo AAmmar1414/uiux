f4678573fc85fbdd13cdfbd9065e4d9c7baa90a7
AMMAR IQBAL <147102569+AAmmar1414@users.noreply.github.com>
2025-02-07 15:09:16 +0500

    updat
    update

--- my-app/src/app/Product/page.tsx
+++ my-app/src/app/Product/page.tsx
@@ -3,7 +3,7 @@ import ProductGird from "../components/Productgrid";
 import Instagram from "../components/Instagram";
 
 
-export default function Home() {
+export default function Product() {
     return (
         <div>
            <ProductGird/>
