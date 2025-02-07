5f7828f9abf9a373e3312aa91a1d5dc4b79aa0e0
AMMAR IQBAL <147102569+AAmmar1414@users.noreply.github.com>
2025-02-07 15:47:28 +0500

    update
    update

--- my-app/src/sanity/env.ts
+++ my-app/src/sanity/env.ts
@@ -2,13 +2,18 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-20'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET="production",
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID=="3uzipn5o",
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
+
+export const _AUTH_TOKEN= assertValue(
+  process.env.NEXT_PUBLIC_SANITY_AUTH_TOKEN="skruz0xMF3Uh9MJYf2hQwTQgndfO2L9g1MVzbaULK5QiyYypLgSUHNxkqHKX3zvYZDWTH5U1qA8xcsCL6bchGdSDkafPS9pk3AN6RT8HhItif4f2eVcsDTUqjO8u0bSmMdRPrOl9wtI7vRstHrYzcYeiLy86pOlPz7t1QhP68FLv5N9R4Cox",
+  'Missing environment variable:NEXT_PUBLIC_SANITY_AUTH_TOKEN '
 )
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
--- my-app/src/sanity/lib/client.ts
+++ my-app/src/sanity/lib/client.ts
@@ -3,8 +3,9 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
+  projectId:"3uzipn5o",
+  dataset:"production",
+  token:"skruz0xMF3Uh9MJYf2hQwTQgndfO2L9g1MVzbaULK5QiyYypLgSUHNxkqHKX3zvYZDWTH5U1qA8xcsCL6bchGdSDkafPS9pk3AN6RT8HhItif4f2eVcsDTUqjO8u0bSmMdRPrOl9wtI7vRstHrYzcYeiLy86pOlPz7t1QhP68FLv5N9R4Cox",
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
